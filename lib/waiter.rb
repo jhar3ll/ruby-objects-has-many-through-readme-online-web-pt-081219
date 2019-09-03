1daea3dd8ea69ecb0f0a57e4987f721a7a83b939
Jharell Chambers <jharell.chambers@selu.edu>
2019-09-03 00:09:21 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -13,7 +13,8 @@ class Waiter
     @@all
   end 
   
-  def new_meal(customer, total, tip)
-    Meal.new()
+  def new_meal(customer, total, tip=0)
+    Meal.new(self, customer, total, tip)
+  end
 
 end
